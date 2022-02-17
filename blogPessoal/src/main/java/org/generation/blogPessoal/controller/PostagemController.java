68dcefbe764df5efbbfdb647cad981ab1d8b10e1
TomasAlric <tomas.alricc@gmail.com>
2022-02-17 10:43:03 -0300

    Implementando método findByTitulo

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -26,5 +26,9 @@ public class PostagemController {
         return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+    }
 
 }
