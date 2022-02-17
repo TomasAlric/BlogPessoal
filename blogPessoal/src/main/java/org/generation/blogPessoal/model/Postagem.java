37b12f0a21cc241242e03297473e9a3d2ca17a41
TomasAlric <tomas.alricc@gmail.com>
2022-02-17 04:15:51 -0300

    Criando entidade postagem

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,28 @@
+package org.generation.blogPessoal.model;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String titulo;
+
+    @NotNull
+    @Size(min = 10, max = 500)
+    private String texto;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date data = new java.sql.Date(System.currentTimeMillis());
+}
