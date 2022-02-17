94814339d296c0d65e5803d8763ab121b90f2417
TomasAlric <tomas.alricc@gmail.com>
2022-02-17 04:46:40 -0300

    Implementando método findAll

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,10 +1,15 @@
 package org.generation.blogPessoal.controller;
 
+import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/postagens")
@@ -13,6 +18,11 @@ public class PostagemController {
 
     @Autowired
     private PostagemRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Postagem>> GetAll(){
+        return ResponseEntity.ok(repository.findAll());
+    }
 
 
 }
