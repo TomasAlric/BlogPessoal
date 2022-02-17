dc99e639934cf0130d1c2400f3d3db025ce730bc
TomasAlric <tomas.alricc@gmail.com>
2022-02-17 10:51:45 -0300

    Implementando método Post

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -3,6 +3,7 @@ package org.generation.blogPessoal.controller;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,18 +18,23 @@ public class PostagemController {
     private PostagemRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<Postagem>> GetAll(){
+    public ResponseEntity<List<Postagem>> GetAll() {
         return ResponseEntity.ok(repository.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Postagem> GetById(@PathVariable long id){
+    public ResponseEntity<Postagem> GetById(@PathVariable long id) {
         return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/titulo/{titulo}")
-    public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+    public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
         return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+    }
+
+    @PostMapping
+    public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
     }
 
 }
